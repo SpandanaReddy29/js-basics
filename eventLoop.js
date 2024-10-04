987325eccd749f399eefd643f3f7d5b72462755e
Apple <apple@Apples-MacBook-Pro.local>
2024-10-04 08:27:52 +0530

    Added event loop

---
+++ eventLoop.js
@@ -0,0 +1,3 @@
+
+//The Event Loop in JavaScript is a mechanism responsible for managing asynchronous behavior in a single-threaded environment.
+//It acts like a traffic controller, ensuring tasks are executed in an orderly manner by processing pending tasks in queues (microtasks and macrotasks)
