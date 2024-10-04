8419f44e686235e786c8b3bdff38e04da2fef487
spandana <salla.spandana@gmail.com>
2024-10-04 09:36:22 +0530

    Changed Author

--- eventLoop.js
+++ eventLoop.js
@@ -1,3 +1,6 @@
 
-//The Event Loop in JavaScript is a mechanism responsible for managing asynchronous behavior in a single-threaded environment.
-//It acts like a traffic controller, ensuring tasks are executed in an orderly manner by processing pending tasks in queues (microtasks and macrotasks).
+//The Event Loop in JavaScript is a mechanism responsible for managing asynchronous behavior in a single-threaded environment
+//It acts like a traffic controller, ensuring tasks are executed in an orderly manner by processing pending tasks in queues (microtasks and macrotasks)
+
+//he event loop is necessary to handle asynchronous operations in JavaScript effectively.
+//It manages task queues and microtask queues to ensure that tasks are executed efficiently without blocking the main thread.
