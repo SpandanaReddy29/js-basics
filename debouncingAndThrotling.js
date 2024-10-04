14eb477bdc80eed715effd5c28a79fdd2fe966b8
spandana <salla.spandana@gmail.com>
2024-10-04 15:23:50 +0530

    Added Debouncing and Throtling

---
+++ debouncingAndThrotling.js
@@ -0,0 +1,8 @@
+                //******************** DEBOUNCING ********************//
+//Debouncing limits the execution of a function call and waits for certain amount of time befor running it again.
+//The JavaScript code uses lodash's _.debounce function to create a debounced version of a callback function
+
+
+                //******************** THROTLING ********************//
+//Throtling is a technique to limit the execution of event handler function, even when this event triggers continously due to event actions
+//The JavaScript code uses lodash's _.throttle function to create a throttled version of a callback function.
